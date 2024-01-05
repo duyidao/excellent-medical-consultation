d144778b373a8ffa69aea8dff39eff7137d01190
v_duyilin <v_duyilin@baidu.com>
2024-01-05 15:46:50 +0800

    feat: 类型定义

--- src/types/user.d.ts
+++ src/types/user.d.ts
@@ -18,3 +18,26 @@ export type CodeType = 'login' | 'register' | 'changeMobile' | 'forgetPassword' 
 export type Code = {
     code: string
 }
+
+// 个人信息
+type OmitUser = Omit<User, 'token'>
+export type UserInfo = OmitUser & {
+  /** 关注 */
+  likeNumber: number
+  /** 收藏 */
+  collectionNumber: number
+  /** 积分 */
+  score: number
+  /** 优惠券 */
+  couponNumber: number
+  orderInfo: {
+    /** 待付款 */
+    paidNumber: number
+    /** 待发货 */
+    receivedNumber: number
+    /** 待收货 */
+    shippedNumber: number
+    /** 已完成 */
+    finishedNumber: number
+  }
+}
