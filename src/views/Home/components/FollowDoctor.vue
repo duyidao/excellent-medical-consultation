e349a022aacf606b3c371a5a212de1c82d7866d5
v_duyilin <v_duyilin@baidu.com>
2024-01-21 16:43:06 +0800

    feat: 首页轮播图width宽度根据页面宽度适配实现

--- src/views/Home/components/FollowDoctor.vue
+++ src/views/Home/components/FollowDoctor.vue
@@ -1,5 +1,23 @@
 <script setup lang="ts">
 import DoctorCard from './DoctorCard.vue'
+import { onMounted, onUnmounted, ref } from 'vue'
+import { useWindowSize } from '@vueuse/core'
+
+const { width } = useWindowSize()
+
+// const width = ref(0)
+// const setWidth = () => width.value = window.innerWidth // 获取屏幕宽度
+
+// // 页面加载，给窗口绑定折叠事件
+// onMounted(() => {
+//     setWidth()
+//     window.addEventListener('resize', setWidth)
+// })
+// // 页面卸载，给窗口取消绑定折叠事件
+// onUnmounted(() => {
+//     window.removeEventListener('resize', setWidth)
+// })
+
 </script>
 
 <template>
@@ -10,7 +28,9 @@ import DoctorCard from './DoctorCard.vue'
         </div>
         <div class="body">
             <!-- swipe 组件 -->
-            <van-swipe :width="150" :show-indicators="false" :loop="false">
+            <van-swipe :width="(150 / 375) * width"
+                :show-indicators="false"
+                :loop="false">
                 <van-swipe-item v-for="item in 5"
                     :key="item">
                     <doctor-card />
