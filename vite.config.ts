cd47298586ac85d8a7b189032c5dc7061ed808a2
平安喜乐 <2014567296@qq.com>
2024-05-02 15:05:44 +0800

    build

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/excellent-medical-consultation',
+  publicPath: '/excellent-medical-consultation',
   plugins: [
     vue(),
     createSvgIconsPlugin({
